3ff6c95c3525e9ba0cf7d91c0a783b46a73e22e3
Tilen Pintaric <tilen.pintaric@direct4.me>
2021-12-27 12:56:27 +0100

    Revert "Updating gradle for publishing. Random test"
    This reverts commit af98952115671f7e0e72106b4534db75c19d4f11.

---
+++ ChoiceSDK/choicesdk-analytics/src/main/java/at/bluesource/choicesdk/analytics/factory/AnalyticsFactory.kt
@@ -0,0 +1,42 @@
+package at.bluesource.choicesdk.analytics.factory
+
+import android.app.Activity
+import android.content.Context
+import androidx.annotation.RequiresPermission
+import at.bluesource.choicesdk.analytics.common.Analytics
+import at.bluesource.choicesdk.analytics.gms.FirebaseAnalytics
+import at.bluesource.choicesdk.analytics.hms.HuaweiAnalytics
+import at.bluesource.choicesdk.core.MobileService
+import at.bluesource.choicesdk.core.MobileServicesDetector
+
+/**
+ * Analytics factory, uses [MobileServicesDetector] to get instance of [Analytics]
+ * Automatically decides if GMS or HMS should be used.
+ *
+ * Throws [UnsupportedOperationException] if no underlying api has been found.
+ *
+ * @see at.bluesource.choicesdk.analytics.gms.FirebaseAnalytics
+ * @see at.bluesource.choicesdk.analytics.hms.HuaweiAnalytics
+ */
+class AnalyticsFactory {
+    companion object {
+
+        @Throws(UnsupportedOperationException::class)
+        @RequiresPermission(allOf = ["android.permission.ACCESS_NETWORK_STATE", "android.permission.INTERNET", "android.permission.WAKE_LOCK"])
+        fun getAnalytics(context: Context): Analytics {
+            return when (MobileServicesDetector.getAvailableMobileService()) {
+                MobileService.GMS -> FirebaseAnalytics.getInstance(context)
+                MobileService.HMS -> HuaweiAnalytics.getInstance(context)
+            }
+        }
+
+        @Throws(UnsupportedOperationException::class)
+        @RequiresPermission(allOf = ["android.permission.ACCESS_NETWORK_STATE", "android.permission.INTERNET", "android.permission.WAKE_LOCK"])
+        fun getAnalytics(activity: Activity): Analytics {
+            return when (MobileServicesDetector.getAvailableMobileService()) {
+                MobileService.GMS -> FirebaseAnalytics.getInstance(activity)
+                MobileService.HMS -> HuaweiAnalytics.getInstance(activity)
+            }
+        }
+    }
+}
